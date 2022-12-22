80f524eba209af5a30ba8723a46d70df630fc1a0
jt31415 <jeremytoddyu@gmail.com>
2022-12-21 19:06:26 -0800

    Add main loop

--- termicraft.py
+++ termicraft.py
@@ -1,1 +1,29 @@
-import argparse
+from msvcrt import kbhit, getch
+import os
+
+from colors import *
+
+def cls(n = 0):
+    if n==0:
+        os.system('cls')
+    else:
+        print('\b \b'*n,end='')
+
+def draw_screen():
+    return ""
+
+n=0
+running=True
+while running:
+    
+    if kbhit():
+        key = getch()
+        try:
+            key = key.decode()
+        except:
+            pass
+
+    final = draw_screen()
+    cls(n)
+    print(final, end = "")
+    n=len(final)
