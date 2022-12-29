f1cc84a0a4fd99325fc228f854e8ee6682eea5a4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-29 10:36:34 -0800

    Initial screen drawing

--- termicraft.py
+++ termicraft.py
@@ -6,11 +6,16 @@ import random
 
 from colors import *
 
+size=width,height=(100,30)
+
 def cls(n = 0):
     if n==0:
         os.system('cls')
     else:
         print('\b \b'*n,end='')
+
+def cursor_to(x, y):
+    print("\033[%d;%dH" % (y, x), end='')
 
 def overwrite(new, n):
     final = new
@@ -21,14 +26,47 @@ def overwrite(new, n):
         length = n
     else:
         length = len_new
-
-    final += '\b'*length
+    
+    # move cursor back to the top left corner
+    final += "\033[H"
 
     print(final, end='')
     return length
 
+def left(text, line):
+    return text + line[len(text):]
+
+def right(text, line):
+    return line[:-len(text)] + text
+
+def center(text, line):
+    where = (len(line)-len(text))//2
+    return line[:where] + text + line[where+len(text):]
+
+
 def draw_screen():
-    return ""
+    lines = [' '*width for i in range(height)]
+
+    if state==MAIN:        
+
+        # fps
+        lines[0] = left(f'FPS:{fps:.2f}',lines[0])
+
+        # action
+        lines[0] = center("Breaking stone",lines[0])
+
+        for y, line in enumerate(lines):
+
+            for x, char in enumerate(lines[y]):
+                pass
+
+    return "\n".join(lines)
+
+MAIN, INV, MENU = range(3)
+state = MAIN
+fps = 60.0
+
+cls()
 
 n=0
 running=True
